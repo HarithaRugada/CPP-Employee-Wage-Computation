728c7e306e5b16961e2e5e677a7e60ff8185ebab
HarithaRugada <haritharugada@gmail.com>
2020-08-19 16:27:14 +0530

    ADDED : Code to reach a condition of 100 working hours or 20 days

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -6,35 +6,40 @@ int WAGE_PER_HOUR = 20;
 int FULL_DAY_HOURS = 8;
 int PART_TIME_HOURS = 4;
 int WORKING_DAYS_PER_MONTH = 20;
-
+int MAXIMUM_WORKING_HOURS = 100;
 int main()
 {
-    int employeeHours, totalWage = 0;
+    int employeeHours, totalWage = 0, totalWorkingHours = 0;
     cout << "Welcome to Employee Wage Computation Problem" << endl;
     srand(time(0));
 
-    for(int day = 1; day <= WORKING_DAYS_PER_MONTH; day++)
+    for(int day = 1; day <= WORKING_DAYS_PER_MONTH && totalWorkingHours <= MAXIMUM_WORKING_HOURS; )
     {
         int status = rand() % 3;
         switch(status)
         {
             case 0 :
-                    cout << "Employee is present for full-time on Day > " << day << endl;
                     employeeHours = FULL_DAY_HOURS;
                     break;
 
             case 1 :
-                    cout << "Employee is present for part-time on Day > " << day << endl;
                     employeeHours = PART_TIME_HOURS;
                     break;
 
             default :
-                    cout << "Employee is absent on Day > " << day << endl;
                     employeeHours = 0;
         }
-        cout << "Employee Daily Wage is " << WAGE_PER_HOUR * employeeHours << " on Day > " << day << endl;
-        totalWage += WAGE_PER_HOUR * employeeHours;
+        if((totalWorkingHours + employeeHours) <= MAXIMUM_WORKING_HOURS)
+        {
+            cout << "Employee Daily Wage is " << WAGE_PER_HOUR * employeeHours << " on Day > " << day << endl;
+            totalWage += WAGE_PER_HOUR * employeeHours;
+            totalWorkingHours += employeeHours;
+            day++;
+        }
+        if(totalWorkingHours == MAXIMUM_WORKING_HOURS)
+            break;
     }
-    cout << "Total wage for the month > " << totalWage;
+    cout << "Total wage for the month > " << totalWage << endl;
+    cout << "Total working hours > " << totalWorkingHours;
     return 0;
 }
