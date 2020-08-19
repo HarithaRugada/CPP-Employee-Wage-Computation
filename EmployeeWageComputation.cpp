10e187840fa8fc912d5475d7a6b09d9358674d3a
HarithaRugada <haritharugada@gmail.com>
2020-08-19 12:59:56 +0530

    ADDED : Code for part time employee

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
 int WAGE_PER_HOUR = 20;
-int FULL_DAY_HOUR = 8;
+int FULL_DAY_HOURS = 8;
+int PART_TIME_HOURS = 4;
 
 int main()
 {
+    int employeeHours = 0;
     cout << "Welcome to Employee Wage Computation Problem" << endl;
     srand(time(0));
-    int result = rand() % 2;
-    (result == 1) ? cout << "Employee is present" << endl : cout << "Employee is absent" << endl;
-    if(result == 1)
-        cout << "Employee Daily Wage is > " << WAGE_PER_HOUR * FULL_DAY_HOUR;
+    int status = rand() % 3;
+
+    switch(status)
+    {
+        case 0 :
+                cout << "Employee is present for full-time" << endl;
+                employeeHours = FULL_DAY_HOURS;
+                break;
+
+        case 1 :
+                cout << "Employee is present for part-time" << endl;
+                employeeHours = PART_TIME_HOURS;
+                break;
+
+        default :
+                cout << "Employee is absent" << endl;
+    }
+
+    cout << "Employee Daily Wage is > " << WAGE_PER_HOUR * employeeHours;
     return 0;
 }
