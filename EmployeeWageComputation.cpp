1b64db5ccc243ffe96f55a1a197f63164a5f98a4
HarithaRugada <haritharugada@gmail.com>
2020-08-19 14:34:56 +0530

    ADDED : Code to calculate monthly wage

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -5,30 +5,36 @@ using namespace std;
 int WAGE_PER_HOUR = 20;
 int FULL_DAY_HOURS = 8;
 int PART_TIME_HOURS = 4;
+int WORKING_DAYS_PER_MONTH = 20;
 
 int main()
 {
-    int employeeHours = 0;
+    int employeeHours, totalWage = 0;
     cout << "Welcome to Employee Wage Computation Problem" << endl;
     srand(time(0));
-    int status = rand() % 3;
 
-    switch(status)
+    for(int day = 1; day <= WORKING_DAYS_PER_MONTH; day++)
     {
-        case 0 :
-                cout << "Employee is present for full-time" << endl;
-                employeeHours = FULL_DAY_HOURS;
-                break;
+        int status = rand() % 3;
+        switch(status)
+        {
+            case 0 :
+                    cout << "Employee is present for full-time on Day > " << day << endl;
+                    employeeHours = FULL_DAY_HOURS;
+                    break;
 
-        case 1 :
-                cout << "Employee is present for part-time" << endl;
-                employeeHours = PART_TIME_HOURS;
-                break;
+            case 1 :
+                    cout << "Employee is present for part-time on Day > " << day << endl;
+                    employeeHours = PART_TIME_HOURS;
+                    break;
 
-        default :
-                cout << "Employee is absent" << endl;
+            default :
+                    cout << "Employee is absent on Day > " << day << endl;
+                    employeeHours = 0;
+        }
+        cout << "Employee Daily Wage is " << WAGE_PER_HOUR * employeeHours << " on Day > " << day << endl;
+        totalWage += WAGE_PER_HOUR * employeeHours;
     }
-
-    cout << "Employee Daily Wage is > " << WAGE_PER_HOUR * employeeHours;
+    cout << "Total wage for the month > " << totalWage;
     return 0;
 }
