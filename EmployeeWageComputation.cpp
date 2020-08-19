044ca0fe089b6b54b61bc6491916d4fcd10230d9
HarithaRugada <haritharugada@gmail.com>
2020-08-19 12:17:56 +0530

    ADDED : Code to check whether the employee is present or not

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-    cout << "Welcome to Employee Wage Computation Problem";
+    cout << "Welcome to Employee Wage Computation Problem" << endl;
+    srand(time(0));
+    int result = rand() % 2;
+    (result == 1) ? cout << "Employee is present" : cout << "Employee is absent";
 }
