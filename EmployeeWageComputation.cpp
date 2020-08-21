8bc09f7148ca86e67ecaadadf658469f1189adcb
HarithaRugada <haritharugada@gmail.com>
2020-08-21 17:35:42 +0530

    REFACTORED : Code to manage wage for multiple companies with one EmpWageBuilder

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int FULL_DAY_HOURS = 8;
 int PART_TIME_HOURS = 4;
 
-struct EmpWageBuilder
+struct CompanyEmpWage
 {
     string companyName;
     int wagePerHour = 0, workingDaysPerMonth = 0, maximumWorkingHours = 0, employees = 0, months = 0;
 
-    EmpWageBuilder(string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months)
+    void EmpWageBuilder(string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months)
     {
         this -> companyName = companyName;
         this -> wagePerHour = wagePerHour;
@@ -30,54 +30,53 @@ struct EmpWageBuilder
         fileStream.open("employeeDetails.csv", ios::out | ios::app);
         srand(time(0));
 
-            for(int monthCount = 1; monthCount <= months; monthCount++)
+        for(int monthCount = 1; monthCount <= months; monthCount++)
+        {
+            cout << "Month > " << monthCount << endl;
+            for(int employeeCount = 1; employeeCount <= employees; employeeCount++)
             {
-                cout << "Month > " << monthCount << endl;
-                for(int employeeCount = 1; employeeCount <= employees; employeeCount++)
+                cout << "Employee ID > " << employeeCount << endl;
+                int day = 1;
+                int totalWorkingHours = 0;
+
+                while(day <= workingDaysPerMonth && totalWorkingHours < maximumWorkingHours)
                 {
-                    cout << "Employee ID > " << employeeCount << endl;
-                    int day = 1;
-                    int totalWorkingHours = 0;
-
-                    while(day <= workingDaysPerMonth && totalWorkingHours < maximumWorkingHours)
+                    int status = rand() % 3;
+                    switch(status)
                     {
-                        int status = rand() % 3;
-                        switch(status)
-                        {
-                            case 0 :
-                                employeeHours = FULL_DAY_HOURS;
-                                break;
+                        case 0 :
+                            employeeHours = FULL_DAY_HOURS;
+                            break;
 
-                            case 1 :
-                                employeeHours = PART_TIME_HOURS;
-                                break;
+                        case 1 :
+                            employeeHours = PART_TIME_HOURS;
+                            break;
 
-                            default :
-                                employeeHours = 0;
-                        }
-
-                        if((totalWorkingHours + employeeHours) <= maximumWorkingHours)
-                        {
-                            fileStream << companyName << ",";
-                            fileStream << wagePerHour << ",";
-                            fileStream << workingDaysPerMonth << ",";
-                            fileStream << maximumWorkingHours << ",";
-                            fileStream << monthCount << ",";
-                            fileStream << employeeCount << ",";
-                            fileStream << day << ",";
-                            cout << "Employee Daily Wage is " << wagePerHour * employeeHours << " on Day > " << day << endl;
-                            totalWage += wagePerHour * employeeHours;
-                            totalWorkingHours += employeeHours;
-                            day++;
-                            fileStream << wagePerHour * employeeHours << ",";
-                            fileStream << totalWage << "," << totalWorkingHours << "\n";
-                        }
+                        default :
+                            employeeHours = 0;
                     }
-                    cout << "Total wage for the month > " << totalWage << endl;
-                    cout << "Total working hours > " << totalWorkingHours << endl << endl;
+
+                    if((totalWorkingHours + employeeHours) <= maximumWorkingHours)
+                    {
+                        fileStream << companyName << ",";
+                        fileStream << wagePerHour << ",";
+                        fileStream << workingDaysPerMonth << ",";
+                        fileStream << maximumWorkingHours << ",";
+                        fileStream << monthCount << ",";
+                        fileStream << employeeCount << ",";
+                        fileStream << day << ",";
+                        cout << "Employee Daily Wage is " << wagePerHour * employeeHours << " on Day > " << day << endl;
+                        totalWage += wagePerHour * employeeHours;
+                        totalWorkingHours += employeeHours;
+                        day++;
+                        fileStream << wagePerHour * employeeHours << ",";
+                        fileStream << totalWage << "," << totalWorkingHours << "\n";
+                    }
                 }
+                cout << "Total wage for the month > " << totalWage << endl;
+                cout << "Total working hours > " << totalWorkingHours << endl << endl;
             }
-    
+        }
         fileStream.close();
     }
 };
@@ -92,8 +91,10 @@ int main()
     cout << "Welcome to Employee Wage Computation Problem" << endl;
 
     //string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months
-    EmpWageBuilder abc("abc", 15, 20, 120, 3, 2), xyz("xyz", 20, 20, 150, 2, 3);
-    abc.calculate();
-    xyz.calculate();
+    struct CompanyEmpWage empWage[2];
+    empWage[0].EmpWageBuilder("abc", 15, 20, 120, 3, 2);
+    empWage[0].calculate();
+    empWage[1].EmpWageBuilder("xyz", 20, 20, 150, 2, 3);
+    empWage[1].calculate();
     return 0;
 }
