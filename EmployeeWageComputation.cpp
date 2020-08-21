921c4566c24cdca89426835e544752dbe326e125
HarithaRugada <haritharugada@gmail.com>
2020-08-21 19:23:55 +0530

    ADDED : Using vector for company list

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <fstream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
 int FULL_DAY_HOURS = 8;
@@ -12,17 +13,65 @@ struct CompanyEmpWage
     string companyName;
     int wagePerHour = 0, workingDaysPerMonth = 0, maximumWorkingHours = 0, employees = 0, months = 0;
 
-    void EmpWageBuilder(string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months)
+    public :
+    void companyDetails(string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months);
+    string getCompanyName();
+    int getWagePerHour();
+    int getWorkingDaysPerMonth();
+    int getMaximumWorkingHours();
+    int getEmployees();
+    int getMonths();
+};
+
+void CompanyEmpWage::companyDetails(string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months)
+{
+    this -> companyName = companyName;
+    this -> wagePerHour = wagePerHour;
+    this -> workingDaysPerMonth = workingDaysPerMonth;
+    this -> maximumWorkingHours = maximumWorkingHours;
+    this -> employees = employees;
+    this -> months = months;
+}
+
+string CompanyEmpWage::getCompanyName()
+{
+    return companyName;
+}
+
+int CompanyEmpWage::getWagePerHour()
+{
+    return wagePerHour;
+}
+
+int CompanyEmpWage::getWorkingDaysPerMonth()
+{
+    return workingDaysPerMonth;
+}
+
+int CompanyEmpWage::getMaximumWorkingHours()
+{
+    return maximumWorkingHours;
+}
+
+int CompanyEmpWage::getEmployees()
+{
+    return employees;
+}
+
+int CompanyEmpWage::getMonths()
+{
+    return months;
+}
+
+struct WageComputation
+{
+    vector<CompanyEmpWage> companyWage;
+    void addCompany(CompanyEmpWage companyEmpWage)
     {
-        this -> companyName = companyName;
-        this -> wagePerHour = wagePerHour;
-        this -> workingDaysPerMonth = workingDaysPerMonth;
-        this -> maximumWorkingHours = maximumWorkingHours;
-        this -> employees = employees;
-        this -> months = months;
+        companyWage.push_back(companyEmpWage);
     }
 
-    void calculate()
+    void calculate(CompanyEmpWage companyEmpWage)
     {
         int employeeHours, totalWage = 0;
         fstream fileStream;
@@ -30,16 +79,16 @@ struct CompanyEmpWage
         fileStream.open("employeeDetails.csv", ios::out | ios::app);
         srand(time(0));
 
-        for(int monthCount = 1; monthCount <= months; monthCount++)
+        for(int monthCount = 1; monthCount <= companyEmpWage.getMonths(); monthCount++)
         {
             cout << "Month > " << monthCount << endl;
-            for(int employeeCount = 1; employeeCount <= employees; employeeCount++)
+            for(int employeeCount = 1; employeeCount <= companyEmpWage.getEmployees(); employeeCount++)
             {
                 cout << "Employee ID > " << employeeCount << endl;
                 int day = 1;
                 int totalWorkingHours = 0;
 
-                while(day <= workingDaysPerMonth && totalWorkingHours < maximumWorkingHours)
+                while(day <= companyEmpWage.getWorkingDaysPerMonth() && totalWorkingHours < companyEmpWage.getMaximumWorkingHours())
                 {
                     int status = rand() % 3;
                     switch(status)
@@ -56,25 +105,26 @@ struct CompanyEmpWage
                             employeeHours = 0;
                     }
 
-                    if((totalWorkingHours + employeeHours) <= maximumWorkingHours)
+                    if((totalWorkingHours + employeeHours) <= companyEmpWage.getMaximumWorkingHours())
                     {
-                        fileStream << companyName << ",";
-                        fileStream << wagePerHour << ",";
-                        fileStream << workingDaysPerMonth << ",";
-                        fileStream << maximumWorkingHours << ",";
+                        fileStream << companyEmpWage.getCompanyName() << ",";
+                        fileStream << companyEmpWage.getWagePerHour() << ",";
+                        fileStream << companyEmpWage.getWorkingDaysPerMonth() << ",";
+                        fileStream << companyEmpWage.getMaximumWorkingHours() << ",";
                         fileStream << monthCount << ",";
                         fileStream << employeeCount << ",";
                         fileStream << day << ",";
-                        cout << "Employee Daily Wage is " << wagePerHour * employeeHours << " on Day > " << day << endl;
-                        totalWage += wagePerHour * employeeHours;
+                        cout << "Employee Daily Wage is " << companyEmpWage.getWagePerHour() * employeeHours << " on Day > " << day << endl;
+                        totalWage += companyEmpWage.getWagePerHour() * employeeHours;
                         totalWorkingHours += employeeHours;
                         day++;
-                        fileStream << wagePerHour * employeeHours << ",";
+                        fileStream << companyEmpWage.getWagePerHour() * employeeHours << ",";
                         fileStream << totalWage << "," << totalWorkingHours << "\n";
                     }
                 }
                 cout << "Total wage for the month > " << totalWage << endl;
                 cout << "Total working hours > " << totalWorkingHours << endl << endl;
+                addCompany(companyEmpWage);
             }
         }
         fileStream.close();
@@ -92,9 +142,11 @@ int main()
 
     //string companyName, int wagePerHour, int workingDaysPerMonth, int maximumWorkingHours, int employees, int months
     struct CompanyEmpWage empWage[2];
-    empWage[0].EmpWageBuilder("abc", 15, 20, 120, 3, 2);
-    empWage[0].calculate();
-    empWage[1].EmpWageBuilder("xyz", 20, 20, 150, 2, 3);
-    empWage[1].calculate();
+    struct WageComputation empWageComputation;
+    
+    empWage[0].companyDetails("abc", 15, 20, 120, 3, 2);
+    empWageComputation.calculate(empWage[0]);
+    empWage[1].companyDetails("xyz", 20, 20, 150, 2, 3);
+    empWageComputation.calculate(empWage[1]);
     return 0;
 }
